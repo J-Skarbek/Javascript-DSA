838dc8d63e52362fe9822925f439e113183d8a4c
J-Skarbek <john@snarkmediaagency.com>
2024-07-15 13:51:00 -0500

    Work on recusive problems

--- countdown.js
+++ countdown.js
@@ -20,3 +20,98 @@ countDown(3)
 //     console.log("All done!")
 // }
 
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(base, exp){
+    
+  if (exp === undefined || exp === null) {
+      const error = new Error('No Exponent provided');
+      return error;
+  }
+  
+  if (base === 1) {
+      return 1;
+  }
+  
+  let result;
+
+  
+  while (exp > 1) {
+      result = base * base;
+      console.log(base, result)
+      power(base, exp -= 1);
+      
+  }
+  
+  return result;
+  
+}
+
+// power(5, 5)
+
+
+function powerSolution(base, exponent){
+  if(exponent === 0) return 1;
+  return base * power(base,exponent-1);
+}
+
+// powerSolution(6, 6)
+
+// power(4, 4)
+
+
+// function factorial(int){
+//   if (int < 0) return 0;
+//   if (int <= 1) return 1;
+//   return int * factorial(int - 1);
+// }
+
+// console.log(`Factorial return: ${factorial(10)}`);
+
+// function productOfArray(arr) {
+//   if (arr && arr.length > 0) {
+//     let accumulator;
+//     for (const value of arr) {
+//       if (value === 0) {
+//         accumulator = value;
+//       }
+//       accumulator = accumulator * value;
+//     }
+//   }
+//   productOfArray(arr)
+// }\\
+
+// function productOfArray(arr) {
+//   let accumulator;
+//   // console.log(arr.length)
+//   if (arr.length === 0) {
+//     return 'end of array';
+//   }
+
+//   if (arr[arr.length - 1]) {
+//     return accumulator = arr[arr.length - 1];
+//   } el
+//   console.log(arr[arr.length - 1])
+//   let val = arr.pop();
+//   console.log(`multiply ${val} * ${accumulator}`)
+//   val * accumulator;
+//   return productOfArray(arr);
+// }
+
+// let testArr = [1, 3, 5, 7]
+
+// console.log(productOfArray(testArr));
+
+function productOfArray(arr) {
+  if (arr.length === 0) { 
+    return 1;
+  }
+
+  return arr[0] * productOfArray(arr.slice(1));
+}
+
+let testArr = [1, 3, 5, 7]
+
+console.log(productOfArray(testArr));
