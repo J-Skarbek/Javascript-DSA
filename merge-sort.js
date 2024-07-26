d2052832ef140a66fee80236f868747c96b7396d
J-Skarbek <john@snarkmediaagency.com>
2024-07-26 16:16:48 -0500

    More merge sort practice

--- merge-sort.js
+++ merge-sort.js
@@ -3,8 +3,6 @@ console.log('******MERGESORT*****');
 const arrOne = [2,3,4,5,6,7,7,8,9,10];
 const arrTwo = [0,1,2];
 
-
-
 function merge(arr, secondArr) {
 
   const sortedArray = [];
@@ -32,10 +30,28 @@ function merge(arr, secondArr) {
     j++;
   }
 
-  console.log(sortedArray);
+  // console.log(sortedArray);
   return sortedArray;
 }
 
 merge(arrOne, arrTwo);
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const midpoint =  Math.floor(arr.length / 2);
+  const firstHalf = mergeSort(arr.slice(0, midpoint));
+  const lastHalf = mergeSort(arr.slice(midpoint));
+  console.log(firstHalf);
+  console.log(lastHalf);
+  console.log(merge(firstHalf, lastHalf));
+  return merge(firstHalf, lastHalf);
+
+
+  // console.log(firstHalf);
+  // console.log(lastHalf);
+}
+
+mergeSort(arrOne);
 
 
