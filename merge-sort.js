8b06548922aeb29ae5e17d73e9a61b3cde39a17a
J-Skarbek <john@snarkmediaagency.com>
2024-07-30 11:15:29 -0500

    Merge sort review

--- merge-sort.js
+++ merge-sort.js
@@ -4,27 +4,49 @@ const arrOne = [2,3,4,5,6,7,7,8,9,10];
 const arrTwo = [0,1,2];
 
 function merge(arr, secondArr) {
+  //This function assumes the two arrays passed in are already sorted;
 
+  //Create a new array to put sorted values into
   const sortedArray = [];
+  //create a pointer for the outer loop
   let i = 0;
+  //create a pointer for the inner loop
   let j = 0;
 
+  //This first loop runs as long as i AND j are less than the length of their
+  //respective arrays
   while(i < arr.length && j < secondArr.length) {
+    // if the item in the second array is larger than the value in the first array
+    //we push in the value of the item in the first array, because it is SMALLER, therefore
+    //it is inserted first -- then we increment the value of i by 1 and the loop continues
     if (secondArr[j] > arr[i]) {
       sortedArray.push(arr[i]);
       i++;
     } else {
+      // If the value in the second array is smaller than the value of the pointer in the first array
+      // Then we push in the value of the item in the second array, and increment j by 1
+      // then the loop continues
       sortedArray.push(secondArr[j]);
       j++;
     }
   }
 
+  //These next two loops will only really trigger once the above loop finishes,
+  //add either the first or second array have more values than the other
+  //so if in this code, the while (i < arr.length) will loop over the other values
+  //in arr because there are more values in that array
   while(i < arr.length) {
     // console.log(arr[i])
     sortedArray.push(arr[i]);
+    // Since the arrays passed in are sorted in a similar way (ie both ascending)
+    // and the first loop has completed, the function will now loop through the remaining
+    // values in the first array and push them into the sorted array, as the values
+    // in the arr array are already sorted, and already we know they are higher values
+    // than the values merged into the sorted array
     i++;
   }
 
+  // This loop would only run if the second array has more values in it than the first
   while(j < secondArr.length) {
     sortedArray.push(secondArr[j]);
     j++;
